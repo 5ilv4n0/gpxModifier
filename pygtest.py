d43812e636513f593c3f7791733704654f56ce75
Silvano Wegener <silvano.wegener@googlemail.com>
2012-08-29 17:50:11 +0200

    new content

--- pygtest.py
+++ pygtest.py
@@ -5,9 +5,8 @@ from pyglet.window import mouse
 import os, sys
 
 
-import urllib
-from thread import start_new_thread
-
+import urllib, time
+import thread
  
 class Graphics(object):
     def __init__(self,winX,winY):
@@ -33,9 +32,9 @@ class Graphics(object):
         if center_x == True: center_x='center'
         if center_y == True: center_y='center'
         if center_x == False or center_y == False:         
-            self.textes[ID] = pyglet.text.Label(txt, font_name=font,x=x, y=y, font_size=fsize)           
+            self.textes[ID] = pyglet.text.Label(txt, font_name=font,x=x, y=y, font_size=fsize, color=(0, 0, 0, 200))           
         else: 
-            self.textes[ID] = pyglet.text.Label(txt, font_name=font,x=x, y=y, font_size=fsize, anchor_x=center_x, anchor_y=center_y)
+            self.textes[ID] = pyglet.text.Label(txt, font_name=font,x=x, y=y, font_size=fsize, anchor_x=center_x, anchor_y=center_y,color=(0, 0, 0, 200))
 
 
 
@@ -56,7 +55,18 @@ class Graphics(object):
             points = _concat(_iter_ellipse(x1, y1, x2, y2))
             pyglet.graphics.draw(len(points)/2, pyglet.gl.GL_LINE_LOOP, ('v2f', points))
 
+
         
+def download(url, fileName):
+    webFile = urllib.urlopen(url)
+    content = webFile.read()
+    webFile.close()
+    if not "Error" in content:
+        localFile = open(fileName, 'w')
+        localFile.write(content)
+        localFile.flush()
+        localFile.close()  
+
 
 
 
@@ -66,9 +76,9 @@ class Graphics(object):
 winX			= 1024
 winY			= 768
 viewPort		= [0,0,winX/256,winY/256]
-move			= [0, 0]
+move			= [winX/2, winY/2]
 mouse			= [0, 0]
-coorsPerPixel	= (3422.0/1000000.0, 2039.0/1000000.0)
+coorsPerPixel	= (0.003422/256.0, 0.002039/256.0)
 mapZero			= (9.977000, 53.446250)
 
 
@@ -81,48 +91,52 @@ ourwnd.clsColor(0,0.2,0)
 ourwnd.text('mouseCoorText', 5,winY-20, '', 'Courier New', 12)
 images = {}
 
-def downloadViews(viewPort):
-    for x in xrange(viewPort[0],viewPort[2]+1):
-        for y in xrange(viewPort[1],viewPort[3]+1):
-            lLon = "%.6f" % ((x*coorsPerPixel[0])+mapZero[0])
-            lLat= "%.6f" % ((y*coorsPerPixel[1])+mapZero[1])
-            hLon  = "%.6f" % ((x*coorsPerPixel[0])+mapZero[0]+(coorsPerPixel[0]))
-            hLat = "%.6f" % ((y*coorsPerPixel[1])+mapZero[1]+(coorsPerPixel[1]))
-           
-            fileName = lLon+','+lLat+','+hLon+','+hLat+'.png'
-            pos = [x*256 ,y*256]
-            try: 
-                try:
-                    images[str(x)+":"+str(y)] = pyglet.resource.image(fileName)
-                    images[str(x)+":"+str(y)].blit(pos[0]+move[0], pos[1]+move[1])
-                except pyglet.image.codecs.ImageDecodeException:
-                    pass
-            except pyglet.resource.ResourceNotFoundException: 
-                url = 'http://parent.tile.openstreetmap.org/cgi-bin/export?bbox='+lLon+','+lLat+','+hLon+','+hLat+'&scale=5310&format=png'
-                print "Downloading "+url
-                webFile = urllib.urlopen(url)
-                content = webFile.read()
-                webFile.close()
-                if not "Error" in content:
-                    localFile = open(fileName, 'w')
-                    localFile.write(content)
-                    localFile.close()    
+
+
+
+
+
+
+
+
 
 
 @ourwnd.window.event
 def on_draw():
     ourwnd.cls()
-    ourwnd.color(255,255,255)
-    ourwnd.line(-50+move[0], +move[1], 50+move[0], move[1])
-    ourwnd.line(move[0], -50+move[1], move[0], 50+move[1])
-    viewPort = [-move[0]/256,-move[1]/256,(winX-move[0])/256,(winX-move[1])/256]
-    ourwnd.color()
-
-    downloadViews(viewPort)
+    viewPort = [(-move[0]/256),(-move[1]/256),((winX-move[0])/256),((winX-move[1])/256)]
+    
+    
+    
+    
+    for y in xrange(viewPort[1],viewPort[3]+1):
+        for x in xrange(viewPort[0],viewPort[2]+1):
+            ourwnd.color(0,0.1,0)
+            pos = [x*256 ,y*256]
+			
+            lLon = "%.6f" % ((x*256*coorsPerPixel[0])+mapZero[0])
+            lLat= "%.6f" % ((y*256*coorsPerPixel[1])+mapZero[1])
+            hLon  = "%.6f" % (((x*256*coorsPerPixel[0])+mapZero[0])+(coorsPerPixel[0]*256))
+            hLat = "%.6f" % (((y*256*coorsPerPixel[1])+mapZero[1])+(coorsPerPixel[1]*256))
+            fileName = str(lLon)+","+str(lLat)+","+str(hLon)+","+str(hLat)+'.png'
+            rasterCoor = [x*256+move[0],y*256+move[1]]
             
-                
-                
-                
+            
+            textID = str((x*256*coorsPerPixel[0])+mapZero[0])+":"+str((y*256*coorsPerPixel[1])+mapZero[1])
+            ourwnd.text(textID, rasterCoor[0]+128, rasterCoor[1]+128, fileName,'Arial', 7, False, False, True, True)
+            ourwnd.textes[textID].draw()
+            
+            ourwnd.color()
+            if os.path.exists(fileName):
+                images[str(x)+":"+str(y)] = pyglet.image.load(fileName)
+                images[str(x)+":"+str(y)].blit(pos[0]+move[0], pos[1]+move[1])
+            else:
+                ourwnd.color(0.0,0.0,0.0)
+                ourwnd.rect(rasterCoor[0],rasterCoor[1],x*256+256+move[0],y*256+256+move[1])
+            
+    ourwnd.color(1,1,1)
+    ourwnd.line(-50+move[0], +move[1], 50+move[0], move[1])
+    ourwnd.line(move[0], -50+move[1], move[0], 50+move[1])              
     ourwnd.textes['mouseCoorText'].draw()
 
 
@@ -138,17 +152,30 @@ def on_mouse_motion(x, y, dx, dy):
     mouseLatString = "%.6f" % mouseLat
     ourwnd.textes['mouseCoorText'].text	= "Lon: " + mouseLonString.rjust(11,' ') + " - Lat: " + mouseLatString.rjust(11,' ')
     
+    
 @ourwnd.window.event
 def on_mouse_drag(x, y, dx, dy, buttons, modifiers):
     ourwnd.mousePos = [x, y]
     if buttons == pyglet.window.mouse.RIGHT:
         move[0] += dx
         move[1] += dy
-        
 
 
-
-
+@ourwnd.window.event
+def on_mouse_press(x, y, buttons, modifiers):                    
+    if buttons == pyglet.window.mouse.MIDDLE:
+        viewPort = [(-move[0]/256),(-move[1]/256),((winX-move[0])/256),((winX-move[1])/256)]
+        for y in xrange(viewPort[1],viewPort[3]+1):
+            for x in xrange(viewPort[0],viewPort[2]+1):	
+                lLon = "%.6f" % ((x*256*coorsPerPixel[0])+mapZero[0])
+                lLat= "%.6f" % ((y*256*coorsPerPixel[1])+mapZero[1])
+                hLon  = "%.6f" % (((x*256*coorsPerPixel[0])+mapZero[0])+(coorsPerPixel[0]*256))
+                hLat = "%.6f" % (((y*256*coorsPerPixel[1])+mapZero[1])+(coorsPerPixel[1]*256))				
+                fileName = str(lLon)+","+str(lLat)+","+str(hLon)+","+str(hLat)+'.png'				
+                if not os.path.exists(fileName):
+                    url = 'http://parent.tile.openstreetmap.org/cgi-bin/export?bbox='+lLon+','+lLat+','+hLon+','+hLat+'&scale=5310&format=png'
+                    print lLon+','+lLat+','+hLon+','+hLat
+                    download(url, fileName)
 
 
 pyglet.app.run()
